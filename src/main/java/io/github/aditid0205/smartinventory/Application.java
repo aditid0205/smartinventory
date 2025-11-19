84955f2eec049dfdfba7716d5ae47297833a8844
Aditi <2023mt93194@wilp.bits-pilani.ac.in>
2025-11-19 11:04:11 +1100

    First commit

---
+++ src/main/java/io/github/aditid0205/smartinventory/Application.java
@@ -0,0 +1,13 @@
+package io.github.aditid0205.smartinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
